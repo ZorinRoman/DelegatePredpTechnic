98dec67ba61f8fae41af3aac064623110c34ae86
Зорин <@K218PC1>
2022-04-11 10:24:40 +0300

    Добавьте файлы проекта.

---
+++ DelegatePredpTechnic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePredpTechnic
+{
+    class Program
+    {
+        public delegate int StrDelegate(string s);
+        static void Main(string[] args)
+        {
+            StrDelegate sD = delegate (string str)
+            {
+                if (str == null)
+                    throw new Exception("Строка отсутствует");
+                else
+                    return str.Length;
+            };
+            Console.WriteLine(sD(""));
+            Console.ReadLine();
+        }
+    }
+}
